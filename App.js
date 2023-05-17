97fb1bbf0ff22cfc0e3dd4a2af105b01aef22219
seon5 <wlsl020528@gmail.com>
2023-05-18 00:48:01 +0900

    Location.requestForegroundPermissionsAsync(), reverseGeocodeAsync() 완료

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-//import * as Location from 'expo-location';
+import * as Location from 'expo-location';
 import { StatusBar } from "expo-status-bar";
 import React, {useEffect, useState} from "react";
 import { View, Text, Dimensions, StyleSheet, ScrollView } from 'react-native';
@@ -7,17 +7,32 @@ const {width:SCREEN_WIDTH} = Dimensions.get("window");
 //console.log(SCREEN_WIDTH);
 
 export default function App() {
-  const [location, setLocation]=useState();
-  const [ok, setPk]=useState(true);
-  // const ask = async () => {
-  //   const permission = await Location.requestForegroundPermissionsAsync();
-  //   console.log(permission);
-  // }
+	const [city, setCity]=useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }
+    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
+    const location = await Location.reverseGeocodeAsync(
+      {latitude,longitude}, 
+      {useGoogleMaps:false}
+    );
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <StatusBar style="light"></StatusBar>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled 
